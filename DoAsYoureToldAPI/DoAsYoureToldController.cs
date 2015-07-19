97199bdcf0cdaa209b8ac959948680972c34aa05
Joe Lloyd <josephglloyd@gmail.com>
2015-07-19 12:27:15 +0100

    Added WebAPI project returning a random string.

---
+++ DoAsYoureToldAPI/DoAsYoureToldController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace DoAsYoureToldAPI
+{
+	[RoutePrefix("")]
+    public class DoAsYoureToldController : ApiController
+    {
+		private readonly DoAsYoureToldStore _store;
+
+		public DoAsYoureToldController()
+		{
+			_store = new DoAsYoureToldStore();
+		}
+
+		[HttpGet]
+		[Route("")]
+		public string Get()
+		{
+			return _store.GetRandom();
+		}
+    }
+}
---
+++ DoAsYoureToldAPI/DoAsYoureToldStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAsYoureToldAPI
+{
+	public class DoAsYoureToldStore
+	{
+		private static readonly List<string> _entries = new List<string>
+		{
+			"Stop it",
+			"Tidy up",
+			"Behave",
+			"Be quiet",
+			"Calm down",
+			"Clean your teeth",
+			"Comb your hair",
+			"Don't do that",
+			"Eat your greens",
+			"Go and play",
+			"Go to bed",
+			"Hurry up",
+			"I said NO",
+			"Not now",
+			"Take care",
+			"Wash your face",
+			"Sit down",
+			"Say please and thank you"
+		};
+
+		public string GetRandom()
+		{
+			return _entries[new Random().Next(0, 17)];
+		}
+	}
+}
